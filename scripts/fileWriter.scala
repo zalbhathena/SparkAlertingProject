4270cb9a318e28f21f18cc32c62052dec0eac565
Zal Noshir Bhathena <znb205@login-2-1.local>
2016-08-08 23:43:52 -0400

    removed extraneous file

--- scripts/fileWriter.scala
+++
@@ -1,6 +0,0 @@
-val tickers = sc.textFile("data/allTicks/part-00000").map(x=>x.split(","))
-for(i <- 0 to 100) 
-{
-	Thread.sleep(1000)
-	tickers.filter(x => x(0).toLong <= (i+1)*10 & x(0).toLong > i*10).map(x=>x.mkString(",")).saveAsTextFile("data/streamingExample/tickers_"+i.toString)
-}
